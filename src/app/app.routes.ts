a2b887e3192f0c6be4d24cf09cf9f874798ec3ef
Calvin Xu <calvin.xu.jy@gmail.com>
2024-11-05 14:41:48 -0500

    Set pages titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,19 @@ import { FundTransferComponent } from './fund-transfer/fund-transfer.component';
 import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
 
 export const routes: Routes = [
-  { path: 'account-creation', component: AccountCreationComponent },
-  { path: 'fund-transfer', component: FundTransferComponent },
-  { path: 'transaction-history', component: TransactionHistoryComponent }
+  {
+    path: 'account-creation',
+    title: 'Account Creation',
+    component: AccountCreationComponent
+  },
+  {
+    path: 'fund-transfer',
+    title: 'Fund Transfer',
+    component: FundTransferComponent
+  },
+  {
+    path: 'transaction-history',
+    title: 'Transaction History',
+    component: TransactionHistoryComponent
+  }
 ];
