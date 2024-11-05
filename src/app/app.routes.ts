159c6c63ee3ab8f2601396503a7be79b0df8fa70
Calvin Xu <calvin.xu.jy@gmail.com>
2024-11-05 14:14:51 -0500

    Initialize routes and components for pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AccountCreationComponent } from './account-creation/account-creation.component';
+import { FundTransferComponent } from './fund-transfer/fund-transfer.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'account-creation', component: AccountCreationComponent },
+  { path: 'fund-transfer', component: FundTransferComponent },
+  { path: 'transaction-history', component: TransactionHistoryComponent }
+];
