04c80fdcb0c806284aed09b3abc0775420fb59a1
Calvin Xu <calvin.xu.jy@gmail.com>
2024-11-06 15:20:04 -0500

    Initialize account list component

--- src/app/accounts/account-list/account-list.component.ts
+++ src/app/accounts/account-list/account-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list'
 
 @Component({
   selector: 'app-account-list',
   standalone: true,
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './account-list.component.html',
   styleUrl: './account-list.component.css'
 })
 export class AccountListComponent {
 
 }
+s
