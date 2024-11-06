51a2932b4adc178bc7785a628b93fee07fdd0daf
Calvin Xu <calvin.xu.jy@gmail.com>
2024-11-06 15:09:47 -0500

    Restructure project for account list UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
-import { AccountCreationComponent } from './account-creation/account-creation.component';
-import { FundTransferComponent } from './fund-transfer/fund-transfer.component';
-import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
+import { AccountCreationComponent } from './accounts/account-creation/account-creation.component';
+import { FundTransferComponent } from './accounts/fund-transfer/fund-transfer.component';
+import { TransactionHistoryComponent } from './accounts/transaction-history/transaction-history.component';
+import { AccountListComponent } from './accounts/account-list/account-list.component';
 
 export const routes: Routes = [
+  {
+    path: 'accounts',
+    title: 'Accounts',
+    component: AccountListComponent
+  },
   {
     path: 'account-creation',
     title: 'Account Creation',
@@ -18,5 +24,10 @@ export const routes: Routes = [
     path: 'transaction-history',
     title: 'Transaction History',
     component: TransactionHistoryComponent
+  },
+  {
+    path: '',
+    redirectTo: '/accounts',
+    pathMatch: 'full'
   }
 ];
