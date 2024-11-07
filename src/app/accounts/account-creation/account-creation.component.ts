bad031340ec985923d8f2549ef4a83232a20b7be
Calvin Xu <calvin.xu.jy@gmail.com>
2024-11-07 11:07:40 -0500

    Add Material form components to UI

--- src/app/accounts/account-creation/account-creation.component.ts
+++ src/app/accounts/account-creation/account-creation.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ACCOUNT_TYPES } from '../shared/account-types';
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { Account } from '../shared/account.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account-creation',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormField, MatLabel, MatRadioButton, MatRadioGroup],
   templateUrl: './account-creation.component.html',
   styleUrl: './account-creation.component.css'
 })
 export class AccountCreationComponent {
+  accountTypes = ACCOUNT_TYPES
+  model = new Account(0, 'Test name', 'Chequing', 0)
+  submitted = false
 
+  onSubmit() {
+    this.submitted = true
+  }
 }
