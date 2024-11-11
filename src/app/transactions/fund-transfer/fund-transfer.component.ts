3c49a954c4f990ba470803b10650eb5f9a9b80fc
Calvin Xu <calvin.xu.jy@gmail.com>
2024-11-10 19:54:36 -0500

    Create transfer amount UI

--- src/app/transactions/fund-transfer/fund-transfer.component.ts
+++ src/app/transactions/fund-transfer/fund-transfer.component.ts
@@ -6,16 +6,19 @@ import { MatSelectModule } from '@angular/material/select';
 import { AccountCardComponent } from '../../accounts/account-card/account-card.component';
 import { MatListModule } from '@angular/material/list';
 import { CurrencyPipe } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-fund-transfer',
   standalone: true,
-  imports: [AccountCardComponent, CurrencyPipe, MatFormFieldModule, MatListModule, MatSelectModule],
+  imports: [AccountCardComponent, CurrencyPipe, MatFormFieldModule, MatInputModule, MatListModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './fund-transfer.component.html',
   styleUrl: './fund-transfer.component.css'
 })
 export class FundTransferComponent {
   private accountService = inject(AccountService)
+  private formBuilder = inject(FormBuilder)
   accounts = this.accountService.getAccounts()
 
   leftSelectedName!: string
@@ -23,8 +26,17 @@ export class FundTransferComponent {
   rightSelectedName!: string
   rightAccount!: Account | undefined
 
+  fundTransferForm = this.formBuilder.group({
+    amount: [0, [Validators.required, Validators.min(0.01)]]
+  })
+
+  onSubmit(transferDirection: string) {
+
+  }
+
   changeAccount(side: string) {
     if (side === 'left') {
+      console.log(this.fundTransferForm.value)
       this.leftAccount = this.accountService.getAccountByName(this.leftSelectedName)
     } else if (side === 'right') {
       this.rightAccount = this.accountService.getAccountByName(this.rightSelectedName)
